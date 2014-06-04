32e1045b79b85b0a43908802997c6119d9c25ade
Mark Lovretovich <mlovretovich@gmail.com>
2014-06-04 02:42:15 -0400

    cast width/height of view to float then round up

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/ClientEnrichments.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/ClientEnrichments.scala
@@ -86,7 +86,7 @@ object ClientEnrichments {
     res match {
       case ResRegex(width, height) =>
         try {
-          (width.toInt: JInteger, height.toInt: JInteger).success
+          (width.toFloat.round.toInt: JInteger, height.toFloat.round.toInt: JInteger).success
         } catch {
           case _ => "Field [%s]: view dimensions [%s] exceed Integer's max range".format(field, res).fail
         }
